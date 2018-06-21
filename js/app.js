c4629fafdee7180417830e2635e0add57a8e482b
Dan Vatnik <dan.vatnik@gmail.com>
2018-06-21 15:04:26 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,151 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: 'snippet',
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            videoEmbeddable: true
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $(".search-items").html("");
+          $(".search-items").show();
+          $.each(results.items, function(index, item) {
+            console.log(item);
+              
+            var url = "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=" + item.id.videoId + "&key=" + googleAPIKey;
+            $.ajax({url: url, success: function(result){
+                $.get("item.html", function(data) {
+                    $(".search-items").append(tplawesome(data, [{
+                        "title":item.snippet.title, 
+                        "videoid":item.id.videoId, 
+                        "image":item.snippet.thumbnails.medium.url,
+                        "id":item.id.videoId + "-search",
+                        "channel":item.snippet.channelTitle,
+                        "views": formatViews(result.items[0].statistics.viewCount)
+                    }]));
+                });
+            }});
+          });
+          resetVideoHeight();
+       });
+    });
+
+    $(window).on("resize", resetVideoHeight);
+
+    $( ".sortable" ).sortable({
+        placeholder: "ui-state-default"
+        });
+        $( ".sortable" ).disableSelection();
+});
+
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9/16);
+}
+
+function init() {
+    gapi.client.setApiKey(googleAPIKey);
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
+}
+
+function getSuggestions(){
+    if($("#search").val() == "")
+        return;
+    var url = "https://cors-anywhere.herokuapp.com/" + "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" + encodeURIComponent($("#search").val()).replace(/%20/g, "+")
+    $.ajax({url: url, success: function(result){
+        var suggestions = JSON.parse(result)[1];
+        $(".search-items").show();
+        $(".suggestion-item").show();
+        if($(".search-items").children(".suggestion-item").length == 0){
+            $(".search-items").html("");
+            $.each(suggestions,function(index, item){
+                $.get("suggestions.html", function(data) {
+                    $(".search-items").append(tplawesome(data, [{"suggestion":item}]));
+                });                
+            });
+        }
+        else{
+            currentSuggestions = $(".search-items").children();
+            for (let index = 0; index < currentSuggestions.length; index++) {
+                $(currentSuggestions[index]).children("div").children("p").html(suggestions[index]);
+                $(currentSuggestions[index]).attr("onclick","selectSuggestion('" + suggestions[index] + "')");
+            }
+        }
+        console.log(JSON.parse(result));
+    }});
+}
+
+function selectSuggestion(suggestion){
+    $("#search").val(suggestion);
+    $("#search").submit();
+}
+
+function addToPlaylist(i){
+    console.log(i);
+    console.log($("#" + i).data("title"));
+    item = $("#" + i);
+    $.get("item.html", function(data) {
+        $("#playlist-items").append(tplawesome(data, [{"title":item.data('title'), 
+        "videoid":item.data('videoid'), 
+        "image":item.data('image'),
+        "id":item.data('videoid') + "-playlist",
+        "channel":item.data('channel'),
+        "views":item.data('views')
+    }]));
+    });
+
+    $(".search-items").hide();
+}
+
+function formatViews(val){
+    var precisionVal = Number.parseFloat(val).toPrecision(3);
+
+    if(precisionVal.length == 3)
+        return val;
+    
+    var magnitude = precisionVal[precisionVal.length -1];
+
+    if(magnitude == "3"){
+        return precisionVal[0] + precisionVal[1] + precisionVal[2] + precisionVal[3] + "k";
+    }
+    
+    if(magnitude == "4"){
+        return precisionVal[0] + precisionVal[2] + precisionVal[1] + precisionVal[3] + "k";
+    }
+
+    if(magnitude == "5"){
+        return precisionVal[0] + precisionVal[2] + precisionVal[3] + "k";
+    }
+
+    if(magnitude == "6"){
+        return precisionVal[0] + precisionVal[1] + precisionVal[2] + precisionVal[3] + "M";
+    }
+
+    if(magnitude == "7"){
+        return precisionVal[0] + precisionVal[2] + precisionVal[1] + precisionVal[3] + "M";
+    }
+
+    if(magnitude == "8"){
+        return precisionVal[0] + precisionVal[2] + precisionVal[3] + "M";
+    }
+
+    if(magnitude == "9"){
+        return precisionVal[0] + precisionVal[1] + precisionVal[2] + precisionVal[3] + "B";
+    }
+
+    if(magnitude == "10"){
+        return precisionVal[0] + precisionVal[2] + precisionVal[1] + precisionVal[3] + "B";
+    }
+
+    if(magnitude == "11"){
+        return precisionVal[0] + precisionVal[2] + precisionVal[3] + "B";
+    }
+}
---
+++ js/youtubeAPI.js
@@ -0,0 +1,210 @@
+    var player1Name = 'player1';
+    var player2Name = 'player2';
+    var player1;
+    var player2;
+    var firstLoadOfVideoPlayer1 = true;
+    var firstLoadOfVideoPlayer2 = true;
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+
+    
+    function onYouTubeIframeAPIReady() {
+        player1 = new YT.Player(player1Name, {
+            height: '281',
+            width: '500',
+            videoId: 'kJQP7kiw5Fk',
+            events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+            }
+        });
+
+        player2 = new YT.Player(player2Name, {
+            height: '281',
+            width: '500',
+            videoId: 'HCjNJDNzw8Y',
+            events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+        //event.target.playVideo();
+        if(event.target == player1 && firstLoadOfVideoPlayer1){
+            firstLoadOfVideoPlayer1 = false;
+            event.target.pauseVideo();
+        }
+        else if(event.target == player2 && firstLoadOfVideoPlayer2){
+            firstLoadOfVideoPlayer2 = false;
+            event.target.pauseVideo();
+        }
+
+        getRelatedVideos(event.target.videoId, event.target);
+    }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING) {
+            if(event.target == player1 && firstLoadOfVideoPlayer1){
+                firstLoadOfVideoPlayer1 = false;
+                event.target.pauseVideo();
+            }
+            else if(event.target == player2 && firstLoadOfVideoPlayer2){
+                firstLoadOfVideoPlayer2 = false;
+                event.target.pauseVideo();
+            }
+        }
+        if (event.data == YT.PlayerState.ENDED || event.data == YT.PlayerState.ERROR){
+            setNextVideoFromPlaylistToPlayer(event.target)
+        }
+    }
+
+    function setNextVideoFromPlaylistToPlayer(player){
+        var currentPlayerInfo = "";
+        if(player == player1){
+            currentPlayerInfo = "#player1-info";
+        }
+        else{
+            currentPlayerInfo = "#player2-info";
+        }
+        playlistItems = $("#playlist-items").children();
+        if(playlistItems.length > 0){
+            item = playlistItems[0];
+            setVideoInPlayerPanel(player, $(item).data("videoid"));
+            setVideoInfo(item, currentPlayerInfo);
+            item.remove()
+        }
+    }
+
+    function pauseVideo(player) {
+        player.pauseVideo();
+    }
+
+    function setVideoInPlayerPanel(player, videoId){
+        player.loadVideoById(videoId, 0);
+        getRelatedVideos(videoId, player);
+        if(player == player1)
+            firstLoadOfVideoPlayer1 = true;
+        if(player == player2)
+            firstLoadOfVideoPlayer2 = true;
+    }
+
+
+    function getRelatedVideos(videoId, player){
+        var currentPlayerRelatedVideos = "";
+        var currentPlayerInfo = "";
+        if(player == player1){
+            currentPlayerRelatedVideos = "#related-items-player-1";
+            currentPlayerInfo = "#player1-info";
+        }
+        else{
+            currentPlayerRelatedVideos = "#related-items-player-2";
+            currentPlayerInfo = "#player2-info";
+        }
+
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            relatedToVideoId: videoId,
+            videoEmbeddable: true
+       }); 
+       // execute the request
+       request.execute(function(response) {
+            var results = response.result;
+            $(currentPlayerRelatedVideos).html("");
+            $.each(results.items, function(index, item) {
+                console.log(item);
+                var url = "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=" + item.id.videoId + "&key=" + googleAPIKey;
+                $.ajax({url: url, success: function(result){
+                    $.get("item.html", function(data) {
+                        $(currentPlayerRelatedVideos).append(tplawesome(data, [{"title":item.snippet.title, 
+                        "videoid":item.id.videoId, 
+                        "image":item.snippet.thumbnails.medium.url,
+                        "id":item.id.videoId + "-search",
+                        "channel":item.snippet.channelTitle,
+                        "views": formatViews(result.items[0].statistics.viewCount)
+                    }]));  
+                });
+            }});
+          });
+          resetVideoHeight();
+        });
+    }
+
+    function switchPlayers(){
+        pl1State = player1.getPlayerState();
+        pl2State = player2.getPlayerState();
+
+        if(pl1State == YT.PlayerState.PLAYING){
+            player2.playVideo();
+            //player2.unmute();
+            basculateVolume(player1, player2);
+
+        }
+        else if(pl2State == YT.PlayerState.PLAYING){
+            player1.playVideo();
+            //player1.unmute();
+            basculateVolume(player2, player1);
+        }
+    }
+
+    function basculateVolume(playerEnding, playerStarting){
+        var maxVolume = playerEnding.getVolume();
+        playerStarting.setVolume(maxVolume);
+        setVolumes(playerEnding, playerStarting, 1, maxVolume);
+
+    }
+    
+    function setVolumes(playerEnding, playerStarting, index, maxVolume){
+        if (index == maxVolume) {
+            playerEnding.stopVideo();
+            playerEnding.unMute();
+            setNextVideoFromPlaylistToPlayer(playerEnding);
+            return;
+        } else {
+            playerEnding.setVolume(maxVolume - index);
+            return setTimeout((function() {
+                return setVolumes(playerEnding, playerStarting, index + 1, maxVolume);
+            }), 75);
+  }
+    }
+
+    function setToPlayer(playerNumber){
+        playlistItems = $("#playlist-items").children();
+        if(playlistItems.length > 0){
+            item = playlistItems[0];
+            if(playerNumber == 1){
+                setVideoInPlayerPanel(player1, $(item).data("videoid"));
+                setVideoInfo(item, "#player1-info")
+            }
+            else if(playerNumber == 2){
+                setVideoInPlayerPanel(player2, $(item).data("videoid"));
+                setVideoInfo(item, "#player2-info")
+            }
+            item.remove();
+        }
+    }
+
+    function setVideoInfo(item, playerTag){
+        $(playerTag).html("")
+        $.get("playerInfo.html", function(data) {
+            $(playerTag).append(tplawesome(data, [{
+                "title":$(item).data("title"), 
+                "views": $(item).data("views")
+            }]));
+        });
+    }
